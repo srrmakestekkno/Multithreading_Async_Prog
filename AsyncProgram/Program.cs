c35a9b51792308c882dd5e6cabbd14051910af5e
stian røgeberg <srr007@hotmail.com>
2024-08-20 11:59:39 +0200

    First commit

---
+++ AsyncProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+// Process.Start("notepad.exe", "C:\\Users\\srr00\\Documents\\Utvikling\\Backend\\Udemy\\Multithreading_and_Asynchronous_programming\\helloworld.txt");
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // var app = new Process();
+        // app.StartInfo.FileName = @"notepad.exe";
+        // app.StartInfo.Arguments = "C:\\Users\\srr00\\Documents\\Utvikling\\Backend\\Udemy\\Multithreading_and_Asynchronous_programming\\helloworld.txt";
+
+        // app.Start();
+
+        // app.PriorityClass = ProcessPriorityClass.RealTime;
+        // app.WaitForExit();
+        // Console.WriteLine("No more waiting");
+        Console.Read();
+    }
+}
